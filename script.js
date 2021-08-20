f79916e4a9f375bd0f2ee36ab93d0a827ff26899
Kamil Khan <kamilkhan478@gmail.com>
2021-08-19 22:32:28 -0400

    Final touches

--- script.js
+++ script.js
@@ -13,16 +13,11 @@ btn.forEach(button => {
     button.addEventListener('click', () => {
         let value = button.innerHTML;
         if (value === 'CLEAR') {
-            // arr.splice(0, arr.length);
-            //alert('empty now');
-            //console.log('used 1');
             clear();
             input(arr);
         }
         else if (value === '=') {
             arr.push('');
-            console.log('used 3=');
-            //operate(valueOne, valueTwo, operator);
             equalPressed(expression);
         }
         else {
@@ -33,29 +28,8 @@ btn.forEach(button => {
 });
 
 function input(buttonPressed) {
-    /* if (buttonPressed.length === 0) {
-         // current_span.innerHTML = 0;
-         clear();
-         console.log('if worded');
-         //clear();
-     }*/
-    // else {
 
     for (let i = 0; i <= buttonPressed.length - 1; i++) {
-        //if (buttonPressed[i] === '+' || buttonPressed[i] === '-' || buttonPressed[i] === 'x' || buttonPressed[i] === '÷') {
-
-        /*if (buttonPressed[i] === '=') {
-            valueTwo.replace('=', '');
-            //var ret = "data-123".replace('data-','');
-
-            // remove = from valuetwo
-            //valueTwo    
-            operate(valueOne, valueTwo, operator);
-        }*/
-
-        /*else if (buttonPressed[i] === 'CLEAR') {
-            clear();
-        }*/
 
         if (isNaN(buttonPressed[i]) && buttonPressed[i] !== '=') {
             operator = buttonPressed[i];
@@ -64,47 +38,39 @@ function input(buttonPressed) {
         current_span.innerHTML = buttonPressed.join('');
         valueOne = buttonPressed.slice(0, index).join('');
         valueTwo = buttonPressed.slice(index + 1, buttonPressed.length).join('');
-        expression = `${valueOne}${operator}${valueTwo}`;
-        //  }
+        expression = `${valueOne} ${operator} ${valueTwo}`;
 
-        //valueOne = buttonPressed.join('');
-        /*if (buttonPressed === '=') {
-            operate(valueOne, valueTwo, operator);
-        } */
-
-
-
-        //        operate(valueOne, valueTwo, operator);
-        console.log('----------');
-        console.log('index' + index);
-        console.log('operator' + operator);
-        console.log(`button pressed ${buttonPressed}`);
-        console.log(`value one ${valueOne}`);
-        console.log(`value two ${valueTwo}`);
+        //*****testing values*****
+        // console.log('----------');
+        // console.log('index' + index);
+        // console.log('operator' + operator);
+        // console.log(`button pressed ${buttonPressed}`);
+        // console.log(`value one ${valueOne}`);
+        // console.log(`value two ${valueTwo}`);
     }
 }
 
 function operate(num1, num2, operator) {
     switch (operator) {
-        case ' + ':
-            add(num1, num2);
+        case '+':
+            let a = parseInt(num1);
+            let b = parseInt(num2);
+            add(a, b);
             return answer;
             break;
-        case ' - ':
+        case '-':
             sub(num1, num2);
             return answer;
             break;
-        case ' x ':
+        case 'x':
             multiply(num1, num2);
             return answer;
             break;
-        case ' ÷ ':
+        case '÷':
             divide(num1, num2);
             return answer;
             break;
     }
-    // no ouptut 
-    // move expression to fullExpression and display answer in current
 }
 function clear() {
     arr.splice(0, arr.length);
@@ -125,7 +91,7 @@ function deleted() {
 }
 function add(a, b) {
     answer = a + b;
-    //return console.log(`answer ${answer}`);
+    return answer;
 }
 function sub(a, b) {
     answer = a - b;
